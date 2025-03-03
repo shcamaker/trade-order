c465029b3eb1b0f65aeb229619bc9cb940309238
shcamaker <shen19871216@126.com>
2025-03-03 16:09:14 +0800

    修复编译问题

--- lib/ShipmentManagement/services/platform_file_handler.dart
+++ lib/ShipmentManagement/services/platform_file_handler.dart
@@ -1,24 +1,34 @@
-import 'dart:async';
+import 'dart:html' as html;
+import 'dart:io';
 import 'package:flutter/foundation.dart' show kIsWeb;
+import 'package:path_provider/path_provider.dart';
 import '../../config.dart';
 
 // 导入平台特定的库
 // ignore: unused_import
 import 'platform_file_handler_web.dart' if (dart.library.io) 'platform_file_handler_native.dart';
 
-// 平台无关的接口
-Future<void> downloadFileWeb(List<int> bytes, String fileName) async {
+Future<void> downloadFile(List<int> bytes, String fileName) async {
   if (kIsWeb) {
-    // 实际实现在platform_file_handler_web.dart中
-    await downloadFileWebImpl(bytes, fileName);
+    await downloadFileWeb(bytes, fileName);
+  } else {
+    await downloadFileNative(bytes, fileName);
   }
 }
 
+Future<void> downloadFileWeb(List<int> bytes, String fileName) async {
+  final blob = html.Blob([bytes]);
+  final url = html.Url.createObjectUrlFromBlob(blob);
+  final anchor = html.AnchorElement(href: url)
+    ..setAttribute('download', fileName)
+    ..click();
+  html.Url.revokeObjectUrl(url);
+}
+
 Future<void> downloadFileNative(List<int> bytes, String fileName) async {
-  if (!kIsWeb) {
-    // 实际实现在platform_file_handler_native.dart中
-    await downloadFileNativeImpl(bytes, fileName);
-  }
+  final directory = await getApplicationDocumentsDirectory();
+  final file = File('${directory.path}/$fileName');
+  await file.writeAsBytes(bytes);
 }
 
 // 这些函数在各自的平台特定文件中实现
